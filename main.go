69034aae17381dfc5c69bbb22e2c11a7a9b609e6
Harsh P Bajaj <harsh.rocks@hotmail.com>
2018-10-15 17:59:12 -0500

    Update main.go

--- main.go
+++ main.go
@@ -60,6 +60,18 @@ func loadConfig(file string) (*Config, error) {
 }
 
 func main() {
+	 var conn *grpc.ClientConn
+	  conn, err := grpc.Dial(":7777", grpc.WithInsecure())
+	  if err != nil {
+	    log.Fatalf("did not connect: %s", err)
+	  }
+	  defer conn.Close()
+	  c := api.NewPingClient(conn)
+	  response, err := c.SayHello(context.Background(), &api.PingMessage{Greeting: "foo"})
+	  if err != nil {
+	    log.Fatalf("Error when calling SayHello: %s", err)
+	  }
+	  log.Printf("Response from server: %s", response.Greeting)
 	tmpls["home.html"] = template.Must(template.ParseFiles(templateDir+"home.html", defaultLayout))
 
 	var err error
